02a009b90797c3847451fa3e60c99bfd22f8561c
Ben Simon <benjisimon@gmail.com>
2020-12-01 03:58:53 +0000

    implemented config based script

--- pkg.sh
+++ pkg.sh
@@ -4,18 +4,107 @@
 ## This script is used for packing up a video
 ##
 
-./ffmpeg -y  \
-  -t 15 -i water.mp4 \
-  -i etz_logo.png \
-  -f lavfi -i color=color=0x666666:1920x1080:d=3:rate=23.98 \
-  -f lavfi -i color=color=0xE4E4E4:1920x1080:d=3:rate=23.98 \
+
+# Default Variables
+base_dir=$(dirname $0)
+font_dir=$base_dir/fonts
+debug=off
+
+ffmpeg=ffmpeg
+ffprobe=ffprobe
+
+logo=logo.png
+
+main_video=input.mp4
+main_image_x="main_w-overlay_w-20"
+main_image_y="(main_h-overlay_h-40)"
+main_caption_text="Thanks for watching"
+main_caption_x=20
+main_caption_y="(h-text_h)-40"
+main_caption_font_size=48
+main_caption_font=Lato-Heavy.ttf
+main_caption_color=0x8a2be2
+main_caption_start=0
+main_caption_end=5
+
+pre_duration=3
+pre_image_x="(main_w-overlay_w)/2"
+pre_image_y="(main_h-overlay_h)*.80"
+pre_bg_color=0x666666
+pre_title_text="Title Text"
+pre_title_font='Lato-Heavy.ttf'
+pre_title_x="(w-text_w)/2"
+pre_title_y="(h-text_h)/2"
+pre_title_font_size=48
+pre_title_color=white
+
+post_duration=5
+post_bg_color=0xE4E4E4
+post_title_text="Thanks for watching"
+post_title_font='Lato-Thin.ttf'
+post_title_font_size=65
+post_title_x="(w-text_w)/2"
+post_title_y="(h-text_h)/2"
+post_title_color=0x222222
+
+config=$HOME/.pkgvid.last
+cp /dev/null $config
+
+while [ -n "$1" ] ; do
+  arg=$1 ; shift
+  if [ -f "$arg" ] ; then
+    echo "# $arg" >> $config
+    cat $arg >> $config
+  fi
+  
+  name=$(echo $arg | cut -d= -f1)
+  if [ "$name" != "$arg" ]; then
+     echo $arg >> $config
+  fi
+
+  echo >> $config
+done
+
+. $config
+
+main_video_width=$($ffprobe -show_streams $main_video 2> /dev/null |grep ^width= | cut -d = -f 2)
+main_video_height=$($ffprobe -show_streams $main_video 2> /dev/null |grep ^height= | cut -d = -f 2)
+
+if [ "$debug" = "on" ]  ; then
+   ffmpeg="echo $ffmpeg"
+fi
+
+$ffmpeg -y  \
+ -i $main_video \
+  -i $logo  \
+  -f lavfi -i color=color=$pre_bg_color:${main_video_width}x${main_video_height}:d=$pre_duration \
+  -f lavfi -i color=color=$post_bg_color:${main_video_width}x${main_video_height}:d=$post_duration \
   -filter_complex "\
     [1:v] scale=w=500:h=-1, split [logo_a][logo_b] ; \
-    [0:v][logo_a] overlay=(main_w-overlay_w-20):(main_h-overlay_h-20)[main] ; \
-    [main]drawtext=fontfile=./fonts/Lato-Heavy.ttf: text='Rav Natan Freller': x=20: y=(h-text_h-20): fontsize=48: fontcolor=0x8a2bE2: shadowcolor=black@.8: shadowx=4: shadowy=4: enable='between(t,0,5)'[main]; \
-    [2:v]drawtext=fontfile=./fonts/Pacifico.ttf: text='Etz Hayim': x=(w-text_w)/2: y=h-text_h-40: fontsize=65: fontcolor=white[pre]; \
-    [pre][logo_b] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2 [pre] ; \
-    [3:v]drawtext=fontfile=./fonts/Lato-Thin.ttf: text='www.etzhayim.net': x=(w-text_w)/2: y=(h-text_h)/2: fontsize=65: fontcolor=0x222222[post]; \
+    [0:v][logo_a] overlay=${main_image_x}:${main_image_y} [main] ; \
+    \
+    [main]drawtext=fontfile=$font_dir/$main_caption_font: \
+          text='$main_caption_text': \
+          x=$main_caption_x: y=$main_caption_y: \
+          fontsize=$main_caption_font_size: \
+          fontcolor=$main_caption_color: \
+          shadowcolor=black@.8: shadowx=4: shadowy=4: \
+          enable='between(t,$main_caption_start,$main_caption_end)'[main]; \
+    \
+    [2:v]drawtext=fontfile=$font_dir/$pre_title_font: \
+         text='$pre_title_text': \
+         x='$pre_title_x': y='$pre_title_y': \
+         fontsize=$pre_title_font_size: \
+         fontcolor=$pre_title_color [pre]; \
+    \
+    [pre][logo_b] overlay=x='$pre_image_x':y='$pre_image_y' [pre] ; \
+    \
+    [3:v]drawtext=fontfile=$font_dir/$post_title_font: \
+         text='$post_title_text':\
+         x='$post_title_x': y='$post_title_y': \
+        fontsize=$post_title_font_size: \
+        fontcolor=$post_title_color [post]; \
+   \
     [pre][main][post]concat=n=3 \
   " \
   -vsync 2 \
